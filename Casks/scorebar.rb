91da669a584826425628ecf65838116f3d36643e
csgulati09 <chandeepsinghgulati@gmail.com>
2025-01-09 23:38:10 +0530

    1.0.1

--- Casks/scorebar.rb
+++ Casks/scorebar.rb
@@ -1,7 +1,7 @@
 cask "scorebar" do
-    version "1.0.0"
-    sha256 "2b4f612f685c2ab4f2c8055c1513794538dc15075106560c9980fb749716c794" # this is the sha256 of the zip file of the app.  
-    url "https://github.com/csgulati09/scorebar/releases/download/v1.0.0/ScoreBar.zip"
+    version "1.0.1"
+    sha256 "086a5e5241300df3365a07cd355a766f798f55840d7d25014a85ca869bc41c4e" # this is the sha256 of the zip file of the app.  
+    url "https://github.com/csgulati09/scorebar/releases/download/v1.0.1/ScoreBar.zip"
     name "ScoreBar"
     desc "A sleek macOS app for tracking scores and stats directly from your menu bar."
     homepage "https://github.com/csgulati09/scorebar.git"
